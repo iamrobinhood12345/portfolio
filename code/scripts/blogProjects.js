aefcc5f80a6c9053c7333ca93c2a28037cd7e901
iamrobinhood12345 <swordsshadow@Williams-MacBook-Pro.local>
2016-09-14 12:17:30 -0700

    added dynamic blog articles

---
+++ code/scripts/blogProjects.js
@@ -0,0 +1,14 @@
+var ourLocalData = [
+  {
+    title:        'Project: blog',
+    projectUrl:   'https://github.com/iamrobinhood12345/portfolio',
+    publishedOn:  '2016-09-12',
+    body:         '<p>Working on this website. Under construction!</p>'
+  },
+  {
+    title:        'Project: upcoming!',
+    projectUrl:   'https://github.com/iamrobinhood12345?tab=repositories',
+    publishedOn:  '2016-09-13',
+    body:         '<p>Not yet!</p>'
+  }
+];
