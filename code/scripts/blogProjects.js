bf99a6c473dacafd453737530320132277980793
iamrobinhood12345 <swordsshadow@Williams-MacBook-Pro.local>
2016-09-26 10:45:40 -0700

    added ajax calls, initial load needs to be debugged, displaying both articles and about

--- code/scripts/blogProjects.js
+++ code/scripts/blogProjects.js
@@ -10,5 +10,11 @@ var ourLocalData = [
     projectUrl:   'https://github.com/iamrobinhood12345?tab=repositories',
     publishedOn:  '2016-09-13',
     body:         '<p>Not ready for publication. Project upcoming! Not ready for publication. Project upcoming! Not ready for publication. Project upcoming! Not ready for publication. Project upcoming! Not ready for publication. Project upcoming! Not ready for publication. Project upcoming! Not ready for publication. Project upcoming! Not ready for publication. Project upcoming! Not ready for publication. Project upcoming! Not ready for publication. Project upcoming! Not ready for publication. Project upcoming!</p>'
+  },
+  {
+    title:        'Article: is rna written in html?',
+    publishedOn:  '2016-09-22',
+    projectUrl:   'https://en.wikipedia.org/wiki/Translation_(biology)',
+    body:         '<p>The Atom text/code editor has an awesome package/extension called Minimap. Minimap places a mini-window in Atoms editor that lets you view large sections of your code at a distance. Ever since Ive worked with the minmap package, Ive had this sneaking suspicion that html code looks a lot like rna translation from a distance. For those who arent familiar with genetics, translation is where rna tags code for strands of blocks of protein. So a tag might code for this protein block, then strand it to this protein block, strand it to this protein block, and so on. Finally, you have a long strand of protein blocks attached to an rna tag. In html, we can think of html tags as delivering coded strands of protein blocks of characters, each character following an html tag then being a newly stranded protein block. Though they have an interesting similarity, it is a long shot to say that rna is coded in html. However, these analogies are very interesting. I would not be surprised if computer programmers develop languages that inherit the characteristics of biological code. Will human written code one day rival the code of life? Time will tell...</p>'
   }
 ];
